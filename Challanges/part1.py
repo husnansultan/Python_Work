3739237733c453fbb128e1a393273e7608ae77cc
Husnan Sultan <husnansultan@academytrainee.com>
2019-04-18 16:48:59 +0100

    99 bottles
    99 bottles - used time import, though there are many other eay to solve this problem

---
+++ Challanges/part1.py
@@ -0,0 +1,15 @@
+##99 bottles of beer chalang
+
+import time
+## 99 Bottles of beer on the wall
+## 99 Bottles of beer
+## Take one down, pass it around
+## 98 bottles of beer on the wall
+bottles = 99
+while bottles > 0:
+    print(bottles,"bottles of beer on the wall")
+    print(bottles,"bottles of beer")
+    print("Take one down, pass it around")
+    bottles = bottles - 1
+    print(bottles,"bottles of beer on the wall")
+    time.sleep(1)
